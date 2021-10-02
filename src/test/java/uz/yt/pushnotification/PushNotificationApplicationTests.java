168a4590d57e8b96bbb1b3f22488850af0367a10
abduvohid0131@gmail.com <imueaa0131>
2021-10-02 13:13:46 +0500

    init

---
+++ src/test/java/uz/yt/pushnotification/PushNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.yt.pushnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
